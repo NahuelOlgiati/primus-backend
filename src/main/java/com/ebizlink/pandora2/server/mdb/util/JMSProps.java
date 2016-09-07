6280dfbfde6e838a4cd08d5c817477d2151fffe7
NahuelOlgiati <nahuelolgiati@hotmail.com>
2016-09-07 14:37:49 -0300

    Swagger and dbMsg

--- src/main/java/com/ebizlink/pandora2/server/mdb/util/JMSProps.java
+++
@@ -1,8 +0,0 @@
-package com.ebizlink.pandora2.server.mdb.util;
-
-public final class JMSProps
-{
-	public static final String QUEUE_TYPE = "javax.jms.Queue";
-	public static final String TOPIC_TYPE = "javax.jms.Topic";
-	public static final String CONNECTION_FACTORY = "ConnectionFactory";
-}
